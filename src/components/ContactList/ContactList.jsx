ad926dffa6f300f326cba1666191e66f8b58f90f
Baxiboo <4avramenko.yana@gmail.com>
2025-06-20 17:11:25 +0300

    test

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+import { useSelector } from 'react-redux';
+import { selectContacts } from '../../redux/contactsSlice';
+import { selectNameFilter } from '../../redux/filtersSlice';
+import Contact from '../Contact/Contact';
+import styles from './ContactList.module.css';
+
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  if (visibleContacts.length === 0) {
+    return <p className={styles.noContacts}>No contacts found.</p>;
+  }
+
+  return (
+    <ul className={styles.contactList}>
+      {visibleContacts.map(contact => (
+        <Contact
+          key={contact.id}
+          id={contact.id}
+          name={contact.name}
+          number={contact.number}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
